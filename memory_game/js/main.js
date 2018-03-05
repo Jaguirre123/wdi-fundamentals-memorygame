c9dea9acf090924368091f050f68e064657f15e7
Jesse Aguirre <jesse@jesses-mbp.attlocal.net>
2018-03-05 05:34:10 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,29 @@
-
-
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
 
 
 var cardsInPlay = [];
+
 
 var checkForMatch = function() {
 	if (cardsInPlay.length == 2){
